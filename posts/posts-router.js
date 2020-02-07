d1dc6e388f58decb40d89c518af616048567c416
HuMart <manya01@protonmail.com>
2020-02-07 11:33:35 -0500

    api/posts/update

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -76,7 +76,7 @@ router.get('/:id', (req, res) => {
         .catch(err => {
             res.status(500).json({ error: "The post information could not be retrieved." });
         });
-}) ;
+}) 
 
 
 // get post/id/comments
@@ -104,8 +104,27 @@ router.delete('/:id', (req, res) => {
             res.status(200).json(deleted);            
         })
         .catch(err => {
-            res.status(500).json({ error: "The post could not be removed" });
+            res.status(404).json({ message: "The post with that ID doesn't exist." });
         })
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    Posts.update(id, req.body)
+        .then(post => {
+            // console.log(req.body);
+            if(!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            } else if (!req.body.title || !req.body.contents) {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+            } else
+            res.status(200).json(post)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post information could not be modified." })
+        });
 })
 
 module.exports = router;
